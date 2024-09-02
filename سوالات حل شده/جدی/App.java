d873d4573022bdc2f2402496551e743018464510
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 02:27:21 +0330

    Update App.java

--- سوالات حل شده/جدی/App.java
+++ سوالات حل شده/جدی/App.java
@@ -6,35 +6,35 @@ public class App
     {
         Scanner scan = new Scanner(System.in);
 
-  		int n = scan.nextInt();
+		int n = scan.nextInt();
         int[] arr = new int[n];
-  		for (int i = 0; i < n; i++) {
-  			arr[i] = scan.nextInt();
-  		}
-  		scan.close();
-  		int length = 1;
-    	boolean soudi = false;
-    	boolean nozuli = true;
-    	for (int i = 0; i < n-1; i++) {
-    			if (arr[i] < arr[i+1]) {
-    				if (soudi) {
-    					continue;
-    				} else {
-      				    length++;
-      					soudi = true;
-      					nozuli = false;
-    				}	
-    			}  
-    			else {
-      				if (nozuli) {
-      					continue;
-      				} else {
-      					length++;
-      					nozuli = true;
-      					soudi = false;
-      				}
-    			}
-    	}
-    	System.out.println(length);	
+		for (int i = 0; i < n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		int length = 1;
+		boolean soudi = false;
+		boolean nozuli = true;
+		for (int i = 0; i < n-1; i++) {
+			if (arr[i] < arr[i+1]) {
+				if (soudi) {
+					continue;
+				} else {
+					length++;
+					soudi = true;
+					nozuli = false;
+				}	
+			}
+			else {
+				if (nozuli) {
+					continue;
+				} else {
+					length++;
+					nozuli = true;
+					soudi = false;
+				}
+			}
+		}
+		System.out.println(length);	
     }
 }
