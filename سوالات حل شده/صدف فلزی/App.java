e2465619adea72dd09ee38d21f2d72e09a6e3a1f
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 02:29:55 +0330

    Create App.java

---
+++ سوالات حل شده/صدف فلزی/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+		    int t = scan.nextInt();
+        int w = scan.nextInt();
+        double temp = 0;
+        for (int i = 0; i < w; i++) {
+            temp += 1 / Math.pow(2, i);
+        }
+        double d = t / temp; 
+        System.out.printf("%.4f", d);
+	  }	
+}       
