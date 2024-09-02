1506e2aee6f9d59c846ee1b815b4f5184ef691ab
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-02 19:53:34 +0330

    Update App.java

--- سوالات حل شده/فاکتوریل/App.java
+++ سوالات حل شده/فاکتوریل/App.java
@@ -9,7 +9,7 @@ public class App
         int factorial = 1;
         for (int i = 1; i <= n; i++ ) {
             factorial = factorial*i;
-		    }
+	}
         System.out.println(factorial);
     }
 }
