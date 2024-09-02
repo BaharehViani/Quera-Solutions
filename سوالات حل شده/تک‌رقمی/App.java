3ece7286867cbbe2dc63b74032b77d3d61494eb3
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 02:20:12 +0330

    Create App.java

---
+++ سوالات حل شده/تک‌رقمی/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+      
+        Scanner scan = new Scanner(System.in);
+        long n = scan.nextLong();
+        long sum = 0;
+        while (n > 0 || sum > 9) {
+            if (n == 0) {
+                n = sum;
+                sum = 0;
+            }
+            sum += n % 10;
+            n /= 10;
+        }
+        System.out.println(sum);
+	  }
+}       
