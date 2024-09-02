a72d547f89a867b0428f519815e242f05c47bd9b
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 02:36:19 +0330

    Update App.java

--- سوالات حل شده/کلید چراغ/App.java
+++ سوالات حل شده/کلید چراغ/App.java
@@ -4,18 +4,17 @@ public class App {
 
 	public static void main(String[] args) {
 	  
-		  Scanner scan = new Scanner(System.in);
-		  int n = scan.nextInt();
-		  int x = scan.nextInt();
-		  int count = 0;
-
-		  for (int i = 0; i < n-1; i++) {
-		      int z = scan.nextInt();
-			    if (z != x) {
-				      count++;
-			    }
-			    x = z;
-		  }
-		  System.out.println(count);
-   }
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int x = scan.nextInt();
+		int count = 0;
+		for (int i = 0; i < n-1; i++) {
+			int z = scan.nextInt();
+			if (z != x) {
+				count++;
+			}
+			x = z;
+		}
+		System.out.println(count);
+	}
 }  
