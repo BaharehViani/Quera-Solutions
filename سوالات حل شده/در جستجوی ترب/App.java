3f3b78399bc53055e9d440f7653e776241110ae0
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 02:12:59 +0330

    Create App.java

---
+++ سوالات حل شده/در جستجوی ترب/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int x1 = scan.nextInt();
+        int v1 = scan.nextInt();
+        int x2 = scan.nextInt();
+        int v2 = scan.nextInt();
+        
+        if (v1 == v2) {
+            System.out.println("WAIT WAIT");
+        }
+        else if ((v2 > v1 && x2 > x1) || (v1 > v2 && x1 > x2)) {
+            System.out.println("BORO BORO");
+        }
+        else if ((v1 > v2 && x2 > x1) || (v2 > v1 && x1 > x2)) {
+            System.out.println("SEE YOU");
+        }
+	  } 
+}       
