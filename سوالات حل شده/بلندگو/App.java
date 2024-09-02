38788eae635eb1f89ceeb51dc3f77f6e5d9ccfc7
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 02:21:43 +0330

    Create App.java

---
+++ سوالات حل شده/بلندگو/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        char[] word = str.toCharArray();
+        System.out.println(str);
+        for (int i = 0; i < word.length-1; i++) {
+            for (int j = i; j >= 0; j--) {
+                word[j] = word[i+1];
+            }
+            for (char c : word) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+	  }   
+}             
