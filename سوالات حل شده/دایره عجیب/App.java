f50a8b43ba31621d91d0fd8bfa1ed8f3855238bc
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-02 20:54:05 +0330

    Create App.java

---
+++ سوالات حل شده/دایره عجیب/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+public class App
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        int hasani = 1, count = 1;
+        if (n==k) {
+            System.out.println(count);
+            return;
+        }
+        hasani+=k;
+        while (hasani!=1) {
+            hasani+=k;
+            count++;
+            if (hasani>=n) {
+                hasani-=n;
+            }
+        }
+		    System.out.println(count);
+    }
+} 
