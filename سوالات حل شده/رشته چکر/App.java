5bab383ccabdc9cc60e7bb360402913e24581169
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 01:59:03 +0330

    Update App.java

--- سوالات حل شده/رشته چکر/App.java
+++ سوالات حل شده/رشته چکر/App.java
@@ -5,12 +5,12 @@ public class App {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-		    String str1 = scan.next();
+	String str1 = scan.next();
         String str2 = scan.next();
         if (str1.charAt(0) == str2.charAt(str2.length()-1)) {
             System.out.println("YES");
         } else {
             System.out.println("NO");
         }  
-	  }	
-}       
+    }	
+} 
