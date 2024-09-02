a44c12e8e0daab52f3f29a54e176d47d3140f084
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-02 21:00:19 +0330

    Update App.java

--- سوالات حل شده/خیلی قهوه ای یا باج یا خوش‌خوار!/App.java
+++ سوالات حل شده/خیلی قهوه ای یا باج یا خوش‌خوار!/App.java
@@ -7,42 +7,41 @@ public class App
         Scanner scan = new Scanner(System.in);
 
         int[] numberOfBooks = new int[3];
-		    for (int i = 0; i < 3; i++) {
-			    numberOfBooks[i] = scan.nextInt();
-		    }
-		    scan.close();
-		    boolean[] possible = {false, false , false};
-		    int evens = 0, odds = 0;
-		    for (int i = 0; i < 3; i++) {
-			    if (numberOfBooks[i] % 2 == 0) {
-				    evens++;
-			    } else {
-				      odds++;
-			    }
-		    }
-		    if (odds == 1 && evens == 2) {
-			    for (int i = 0; i < 3; i++) {
-				    if (numberOfBooks[i] % 2 == 1) {
-					    possible[i] = true;
-					    break;
-				    }
-			    }
-		    }
-		    else if (odds == 2 && evens == 1) {
-			    for(int i = 0; i < 3; i++) {
-				    if (numberOfBooks[i] % 2 == 0) {
-					    possible[i] = true;
-					    break;
-				    }
-			    }
-		    }
-		    for (int i = 0; i < 3; i++) {
-			    if (possible[i]) {
-				    System.out.print("YES ");
-			    }
-          else {
-				      System.out.print("NO ");
-			    }
-		    }
-	  }
+	for (int i = 0; i < 3; i++) {
+		numberOfBooks[i] = scan.nextInt();
+	}
+	scan.close();
+	boolean[] possible = {false, false , false};
+	int evens = 0, odds = 0;
+	for (int i = 0; i < 3; i++) {
+		if (numberOfBooks[i] % 2 == 0) {
+			evens++;
+		} else {
+			odds++;
+		}
+	}
+	if (odds == 1 && evens == 2) {
+		for (int i = 0; i < 3; i++) {
+			if (numberOfBooks[i] % 2 == 1) {
+				possible[i] = true;
+				break;
+			}
+		}
+	}
+	else if (odds == 2 && evens == 1) {
+		for(int i = 0; i < 3; i++) {
+			if (numberOfBooks[i] % 2 == 0) {
+				possible[i] = true;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		if (possible[i]) {
+			System.out.print("YES ");
+		} else {
+			System.out.print("NO ");
+		}
+	}
+    }
 }
