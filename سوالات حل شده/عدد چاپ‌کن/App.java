9ff39322739c48262e4b3e6759c082eece85dee1
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-02 20:10:15 +0330

    Create App.java

---
+++ سوالات حل شده/عدد چاپ‌کن/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        int number;
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + ": ");
+            number = Character.getNumericValue(str.charAt(i));
+            for (int j = 0; j < number; j++) {
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+	  } 
+}       
