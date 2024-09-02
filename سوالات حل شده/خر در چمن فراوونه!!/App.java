96bdb8ed1644a5a3340c1e3ab35860c4e7fcb0a5
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 02:04:15 +0330

    Create App.java

---
+++ سوالات حل شده/خر در چمن فراوونه!!/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int l = scan.nextInt();
+        int sum = 0;
+        for (int i = 0; i < l/2; i++) {
+            sum += a+b;
+        }
+        if (l % 2 != 0) {
+            sum += a;
+        } 
+        System.out.println(sum);
+	  } 
+}       
