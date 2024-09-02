62eb157f7bce40a554129832ec48381a8dd23e59
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-02 19:20:48 +0330

    Update App.java

--- سوالات حل شده/چاپ مربع/App.java
+++ سوالات حل شده/چاپ مربع/App.java
@@ -7,14 +7,14 @@ public class App
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         for (int i = 0; i < n; i++ ) {
-			      for (int j = 0 ; j < n; j++ ) {
-				        if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
-					          System.out.print("*");
-				        } else {
-					          System.out.print(" ");
-				        }
-			      }
-			      System.out.println();
-		    }
+		for (int j = 0 ; j < n; j++ ) {
+			if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+				System.out.print("*");
+			} else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
     }
 }
