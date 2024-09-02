8c2d039eab2db41ce5dcae18e22039848cc26ec8
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-02 20:38:54 +0330

    Create App.java

---
+++ سوالات حل شده/جمع اعداد/App.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        BigInteger a = scan.nextBigInteger();
+        BigInteger sum = a;
+        for (int i = 0; i < n-1; i++) {
+            BigInteger b = scan.nextBigInteger();
+            sum = a.add(b);
+            a = sum;
+        }
+        System.out.println(sum);
+	  }   
+}       
