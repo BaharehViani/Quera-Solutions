79e3d75e7a18b7eaaa5ccfd8d172a758ee52eca4
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-02 19:10:42 +0330

    Update App.java

--- سوالات حل شده/اعداد اول/App.java
+++ سوالات حل شده/اعداد اول/App.java
@@ -5,20 +5,20 @@ public class App
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
-      
+
         int a = scan.nextInt();
         int b = scan.nextInt();
-      
+
         for (int i = a; i <= b; i++) {
-            int count = 0;
-            for (int j = 1 ; j <= i ; j++) {
-				        if (i % j == 0) {
-					        count += 1;
-                }    
-			      }
-            if(count == 2) {
-				        System.out.println(i);
-            }
+        	int count = 0;
+            	for (int j = 1 ; j <= i ; j++) {
+			if (i % j == 0) {
+				count += 1;
+                	}    
+	    	}
+            	if(count == 2) {
+			System.out.println(i);
+            	}
         }
         scan.close();
     }
