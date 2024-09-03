f6c53e9905b237cecfec329f940b7f5f554df586
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 19:39:32 +0330

    Update App.java

--- سوالات حل شده/بازگشت از بوستان/App.java
+++ سوالات حل شده/بازگشت از بوستان/App.java
@@ -5,16 +5,14 @@ public class App {
 	public static void main(String[] args) {
 	  
 		Scanner scan = new Scanner(System.in);
-
 		int x1 = scan.nextInt();
 		int y1 = scan.nextInt();
 		int x2 = scan.nextInt();
 		int y2 = scan.nextInt();
-		
 		if (x2 > x1) {
 			System.out.println("Right");
 		} else {
 			System.out.println("Left");
 		}
 	}
-}  
+} 
