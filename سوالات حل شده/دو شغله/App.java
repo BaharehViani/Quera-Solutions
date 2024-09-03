02b69b693e82e6064655bac5e7b746b6c578751f
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 01:06:34 +0330

    Create App.java

--- سوالات حل شده/دو شغله/App.java
+++ سوالات حل شده/دو شغله/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-		    String rooz = scan.next();
+	String rooz = scan.next();
         String[] hafte = {"shanbe","yekshanbe","doshanbe","seshanbe","chaharshanbe","panjshanbe","jome"};
         for (int i = 0; i < hafte.length; i++) {
             if (rooz.equals(hafte[i])) {
