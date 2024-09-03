631773a6c50611c4152bcfa5f84219c9637ce5de
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 00:23:28 +0330

    Update App.java

--- سوالات حل شده/نمایشگاه مجازی/App.java
+++ سوالات حل شده/نمایشگاه مجازی/App.java
@@ -5,26 +5,25 @@ public class App
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
-
-		int k = scan.nextInt();
-		int counter = 1;
-		for (int i = 0; i < 8; i++) {
-			if (i % 2 == 0) {
-				System.out.println("########.......########");
-			}
-			else if (k == 0) {
-				System.out.println("#.....................#");
-			}
-			else if (k == 1) {
-				System.out.println("#ghorfe"+i+"..............#");
-				k = 0;
-			}
-			else {
-				System.out.println("#ghorfe"+counter+".......ghorfe"+(counter+1)+"#");
-				k = k - 2;
-				counter += 2;
-			}
+	int k = scan.nextInt();
+	int counter = 1;
+	for (int i = 0; i < 8; i++) {
+		if (i % 2 == 0) {
+			System.out.println("########.......########");
 		}
-		System.out.println("#######################");
+		else if (k == 0) {
+			System.out.println("#.....................#");
+		}
+		else if (k == 1) {
+			System.out.println("#ghorfe"+i+"..............#");
+			k = 0;
+		}
+		else {
+			System.out.println("#ghorfe"+counter+".......ghorfe"+(counter+1)+"#");
+			k = k - 2;
+			counter += 2;
+		}
+	}
+	System.out.println("#######################");
     } 
 }
