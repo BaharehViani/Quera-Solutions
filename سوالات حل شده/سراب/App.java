d2c6324d7687c9eea9ea92cd422425a6f3e82602
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 00:33:21 +0330

    Create App.java

---
+++ سوالات حل شده/سراب/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            if (x-y == 0 || x-y == 2) {
+                if (x % 2 == 0) {
+                    System.out.println(x+y);
+                } else {
+                    System.out.println(x+y-1);
+                }
+            } 
+            else {
+                System.out.println(-1);
+            }
+        }
+	}
+}
