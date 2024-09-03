41cffb9070bcc2a9fb62ac3a019880abc9c56759
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 00:35:38 +0330

    Create App.java

--- سوالات حل شده/بتایپ/App.java
+++ سوالات حل شده/بتایپ/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-		    String str = scan.next();
+	String str = scan.next();
         StringBuilder sb = new StringBuilder(str);
         
         for (int j = 0; j < sb.length(); j++) {
