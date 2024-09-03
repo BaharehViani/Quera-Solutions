e21f0a20a80abf34d8b411edcb074548eae2df0c
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 19:12:15 +0330

    Create App.java

---
+++ سوالات حل شده/سلام سلام خداحافظ/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+		    int n = scan.nextInt();
+        //fill array
+        String[] name = new String[n];
+        for (int i = 0; i < name.length; i++) {
+            name[i] = scan.next();
+        }
+        // salam
+        for (int i = 1; i < name.length; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.println(name[i] + ": salam " + name[i-j] + "!"); 
+            }
+        }
+        // khodafez
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i] + ": khodafez bacheha!");
+            for (int j = 1; j < name.length-i; j++) {
+                System.out.println(name[j+i] + ": khodafez " + name[i] + "!");
+            }
+        }
+	  }	
+}       
