484905be083bdfb2f8e4c19c7cc7dd3623fe8fd2
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 19:30:07 +0330

    Create App.java

---
+++ سوالات حل شده/خوش‌آمد/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ 
+public class App {
+
+	public static void main(String[] args) {
+	  
+  		Scanner scan = new Scanner(System.in);
+  		int n = scan.nextInt();
+  		System.out.println("Hello CodeCup " + n + "!");
+	 }
+}  
