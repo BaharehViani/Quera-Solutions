737ebf8869cf3c8bbbde0f9e5c359469211d561a
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 00:15:11 +0330

    Create App.java

---
+++ سوالات حل شده/جمع بزرگان/App.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+ 
+public class App
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        BigInteger x = scan.nextBigInteger();
+		    String str = scan.next();
+        BigInteger y = scan.nextBigInteger();
+        scan.close();
+
+        if (str.equals("+")) {
+    			System.out.print(x.add(y));
+    		} else {
+			    System.out.print(x.multiply(y));
+		    }
+    }
+}
