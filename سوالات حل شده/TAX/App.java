b49e9cc738288a72a164498215fa7abee6701be2
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 00:26:50 +0330

    Create App.java

---
+++ سوالات حل شده/TAX/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int income, tax;
+        while (true) {
+            income = scan.nextInt();
+            if (income == 0) {
+                break;
+            }
+            if (income <= 1000000) {
+                System.out.println(income);
+            }
+            else if (income <= 5000000) {
+                tax = income / 10;
+                System.out.println(income - tax);
+            }
+            else {
+                tax = (income * 2) / 10;
+                System.out.println(income - tax);
+            }
+        }
+	} 
+}       
