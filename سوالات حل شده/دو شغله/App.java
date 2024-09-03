a104ed4c1a5f7281cbb5c98131d5a36a67287b52
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 01:06:14 +0330

    Create App.java

---
+++ سوالات حل شده/دو شغله/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class App
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+		    String rooz = scan.next();
+        String[] hafte = {"shanbe","yekshanbe","doshanbe","seshanbe","chaharshanbe","panjshanbe","jome"};
+        for (int i = 0; i < hafte.length; i++) {
+            if (rooz.equals(hafte[i])) {
+                if (i == 6) {
+                    System.out.println("tatil");
+                }
+                else if (i % 2 == 0) {
+                    System.out.println("perspolis");
+                }
+                else {
+                    System.out.println("bahman");
+                }
+            } 
+        }
+    }
+}
