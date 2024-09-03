95630d147b6eaba107bab76be99141ae7752da77
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 00:48:13 +0330

    Create main.py

---
+++ سوالات حل شده/حاج مشتی/main.py
@@ -0,0 +1,9 @@
+travel = input()
+if(travel[0] == "Y"):
+    print("Haji")
+elif(travel[1] == "Y"):
+    print("Karbalaee") 
+elif(travel[2] == "Y"):
+    print("Mashti")
+else:
+    print("Agha")     
