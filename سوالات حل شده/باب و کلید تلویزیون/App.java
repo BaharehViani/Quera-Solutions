2b3c84b3ce43301a41b53c72acfa4dfd309e09ae
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 19:23:35 +0330

    Create App.java

---
+++ سوالات حل شده/باب و کلید تلویزیون/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int initial_channel = scan.nextInt();
+        int k = scan.nextInt();
+        int final_channel;
+        String[] channel = new String[n];
+        for (int i = 0; i < channel.length; i++) {
+            channel[i] = scan.next();
+        }
+        final_channel = initial_channel + k;
+        while (final_channel > n) {
+            final_channel -= n;
+        }
+        System.out.println(channel[final_channel-1]);
+	  }
+}       
