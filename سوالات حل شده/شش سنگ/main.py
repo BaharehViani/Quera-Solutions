db9d3501c16a4affb9b7ced2b502e9e9d612533e
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-03 19:16:21 +0330

    Create main.py

---
+++ سوالات حل شده/شش سنگ/main.py
@@ -0,0 +1,13 @@
+stone = input()
+if(stone == "space"):
+    print("blue")
+elif(stone == "mind"):
+    print("yellow") 
+elif(stone == "reality"):
+    print("red")
+elif(stone == "power"):
+    print("purple")
+elif(stone == "time"):
+    print("green")
+else:
+    print("orange")
