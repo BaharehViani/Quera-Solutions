0ed4fddf4968931041278b7012e15497021158c5
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 00:35:13 +0330

    Create App.java

---
+++ سوالات حل شده/بتایپ/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class App
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+		    String str = scan.next();
+        StringBuilder sb = new StringBuilder(str);
+        
+        for (int j = 0; j < sb.length(); j++) {
+            if (sb.charAt(0) == '=') {
+                sb.deleteCharAt(0);
+                j -= 1;
+            }
+            else if (sb.charAt(j) == '=') {
+                sb.deleteCharAt(j);
+                sb.deleteCharAt(j-1);
+                j -= 2;
+            }
+        }
+        System.out.println(sb);
+    }
+}
