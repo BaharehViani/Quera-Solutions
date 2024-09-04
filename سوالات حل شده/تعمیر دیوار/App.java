f378835c7e483d0b46c0f6738ecfda004cbb4194
Bahareh Viani <111139556+BaharehViani@users.noreply.github.com>
2024-09-04 15:10:05 +0330

    Create App.java

--- سوالات حل شده/تعمیر دیوار/App.java
+++ سوالات حل شده/تعمیر دیوار/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-		    int squareX = scan.nextInt();
+	int squareX = scan.nextInt();
         int squareY = scan.nextInt();
         int r = scan.nextInt();
         int glassX = scan.nextInt();
@@ -16,5 +16,5 @@ public class App {
         } else {
             System.out.println("Parsa");
         }
-	}	
+    }	
 }       
